951fb643f19c6f5961149b82f13463767f848f89
wiz <154702154+owizdom@users.noreply.github.com>
2026-05-21 21:35:43 +0200

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 26
+float_type = 3.0
+bool_type = False
+string_type = "idk"
+list_type = [1,2,3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
