d4f32923aac6fa769f572406e5e6029308b6b70f
wiz <154702154+owizdom@users.noreply.github.com>
2026-05-21 21:44:23 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 23
+print(type(variable))
+variable = 1.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "she"
+print(type(variable))
+variable = [1,3,4]
+print(type(variable))
