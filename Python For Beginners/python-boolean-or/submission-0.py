cff269bc807dfa197ceb3fa6b40b66a9fd4c6878
wiz <154702154+owizdom@users.noreply.github.com>
2026-05-22 05:28:42 +0200

    Add: python-boolean-or - submission-0

---
+++ Python For Beginners/python-boolean-or/submission-0.py
@@ -0,0 +1,11 @@
+a, b, c, d = False, False, True, True
+
+firstLogic = (a or b)
+secondLogic = (b or c)
+thirdLogic = (c or d)
+finalLogic = (a or b or c or d)
+
+print (firstLogic)
+print (secondLogic)
+print (thirdLogic)
+print (finalLogic)
