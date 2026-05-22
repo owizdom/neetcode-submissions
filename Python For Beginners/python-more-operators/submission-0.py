ee7124e07780f92078ee924540aa64260009002b
wiz <154702154+owizdom@users.noreply.github.com>
2026-05-22 05:19:39 +0200

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,11 @@
+a, b, c = 2, 8, 5
+
+product = (a * b) // c
+remainder = (a * b) % c
+power = (a ** b)
+secondPower = ( b ** c)
+
+print (product)
+print (remainder)
+print (power)
+print (secondPower)
